089bb9295e697771dd094000e69c025b4d4fabad
Tarek El Jalkh <tarekeljalkh@gmail.com>
2025-01-09 15:14:41 +0200

    Update edit.blade.php

--- resources/views/invoices/edit.blade.php
+++ resources/views/invoices/edit.blade.php
@@ -10,72 +10,67 @@
             <li class="breadcrumb-item active" aria-current="page">Edit Invoice</li>
         </ol>
     </nav>
-
     <div class="row g-6">
-        <div class="col-md">
-            <div class="card">
-                <h5 class="card-header">Edit Invoice</h5>
-                <div class="card-body">
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <p>There were some problems with your input. Please check the form below:</p>
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
+    <div class="col-12">
+        <div class="card">
+            <h5 class="card-header">Edit Invoice</h5>
+            <div class="card-body">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <p>There were some problems with your input. Please check the form below:</p>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
 
-                    <ul class="nav nav-tabs" id="editInvoiceTabs" role="tablist">
-                        <li class="nav-item" role="presentation">
-                            <button class="nav-link active" id="returns-tab" data-bs-toggle="tab" data-bs-target="#returns"
-                                type="button" role="tab" aria-controls="returns" aria-selected="true">Manage
-                                Returns</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <button class="nav-link" id="additions-tab" data-bs-toggle="tab" data-bs-target="#additions"
-                                type="button" role="tab" aria-controls="additions" aria-selected="false">Add New
-                                Items</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <button class="nav-link" id="payment-status-tab" data-bs-toggle="tab"
-                                data-bs-target="#payment-status" type="button" role="tab"
-                                aria-controls="payment-status" aria-selected="false">Payment Status</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <button class="nav-link" id="invoice-status-tab" data-bs-toggle="tab"
-                                data-bs-target="#invoice-status" type="button" role="tab"
-                                aria-controls="invoice-status" aria-selected="false">Invoice Status</button>
-                        </li>
+                <ul class="nav nav-tabs flex-column flex-sm-row" id="editInvoiceTabs" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="returns-tab" data-bs-toggle="tab" data-bs-target="#returns"
+                            type="button" role="tab" aria-controls="returns" aria-selected="true">Manage Returns</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="additions-tab" data-bs-toggle="tab" data-bs-target="#additions"
+                            type="button" role="tab" aria-controls="additions" aria-selected="false">Add New Items</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="payment-status-tab" data-bs-toggle="tab"
+                            data-bs-target="#payment-status" type="button" role="tab"
+                            aria-controls="payment-status" aria-selected="false">Payment Status</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="invoice-status-tab" data-bs-toggle="tab"
+                            data-bs-target="#invoice-status" type="button" role="tab"
+                            aria-controls="invoice-status" aria-selected="false">Invoice Status</button>
+                    </li>
+                </ul>
+                <div class="tab-content mt-3">
+                    {{-- Manage Returns Tab --}}
+                    <div class="tab-pane fade show active" id="returns" role="tabpanel" aria-labelledby="returns-tab">
+                        @include('invoices.partials.manage-returns')
+                    </div>
 
-                    </ul>
-                    <div class="tab-content" id="editInvoiceTabsContent">
-                        {{-- Manage Returns Tab --}}
-                        <div class="tab-pane fade show active" id="returns" role="tabpanel" aria-labelledby="returns-tab">
-                            @include('invoices.partials.manage-returns')
-                        </div>
+                    {{-- Add New Items Tab --}}
+                    <div class="tab-pane fade" id="additions" role="tabpanel" aria-labelledby="additions-tab">
+                        @include('invoices.partials.add-new-items')
+                    </div>
 
-                        {{-- Add New Items Tab --}}
-                        <div class="tab-pane fade" id="additions" role="tabpanel" aria-labelledby="additions-tab">
-                            @include('invoices.partials.add-new-items')
-                        </div>
+                    {{-- Payment Status Tab --}}
+                    <div class="tab-pane fade" id="payment-status" role="tabpanel" aria-labelledby="payment-status-tab">
+                        @include('invoices.partials.payment-status')
+                    </div>
 
-                        {{-- Payment Status Tab --}}
-                        <div class="tab-pane fade" id="payment-status" role="tabpanel" aria-labelledby="payment-status-tab">
-                            @include('invoices.partials.payment-status')
-                        </div>
-
-                        {{-- Invoice Status Tab --}}
-                        <div class="tab-pane fade" id="invoice-status" role="tabpanel" aria-labelledby="invoice-status-tab">
-                            @include('invoices.partials.invoice-status')
-                        </div>
-
+                    {{-- Invoice Status Tab --}}
+                    <div class="tab-pane fade" id="invoice-status" role="tabpanel" aria-labelledby="invoice-status-tab">
+                        @include('invoices.partials.invoice-status')
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
     {{-- Include Scripts --}}
     @push('scripts')
